a4af0e4b88f210cab929bd1d02c37836bffd996e
Janjaweeds <Villadsen67@hotmail.com>
2020-12-27 19:47:23 +0100

    finished morf

--- Compression/Compression/Top.h
+++ Compression/Compression/Top.h
@@ -4,13 +4,8 @@
 #include "ISensor.h"
 class Top : public sc_module {
 public:
-<<<<<<< HEAD
-    sc_fifo<Package*> fifo;
-    ISensor<Package>* sensor;
-=======
     sc_fifo<Package*>* fifo;
     ISensor<Package*>* sensor;
->>>>>>> 1dc35a4c060c20f2f8396256c23d77ef9bfb31a5
 
     SC_HAS_PROCESS(Top);
     Top(sc_module_name name);
