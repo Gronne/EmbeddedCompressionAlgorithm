41e6a718778d7e5ea650948a5bf9938662263d07
mactanex <malthedj@hotmail.com>
2020-12-27 14:46:28 +0100

    fix kristoffer

--- Compression/Compression/Input.cpp
+++ Compression/Compression/Input.cpp
@@ -8,24 +8,24 @@ Input::Input(sc_module_name name) : sc_module(name) {
 void Input::sensor() {
 	while (true) {
 		FILE* fp_data;
-		char tmp_val;
+		
 		fp_data = fopen(INPUT_FILE_A, "r");
 		fseek(fp_data, 0, SEEK_END);
 		size = ftell(fp_data);
+		char tmp_val[100];
 		rewind(fp_data);
 		Package* data = new Package();
 		std::cout << size << std::endl;
-		for (size_t i = 0; i < size; i++)
-		{
-			if (fscanf(fp_data, "%c", &tmp_val) == EOF) {
-				sc_stop;
-				break;
-			}
-			data->Inputvalue = tmp_val;
-			printf("%c", data->Inputvalue);
-			out.write(data);
-			wait(20, SC_MS);
-		}
 		
+		while (fscanf(fp_data, "%s", tmp_val) != -1)
+		{
+			printf("%s ", tmp_val);
+			//data->Inputvalue = tmp_val;
+			//printf("%s", data->Inputvalue);
+			//out.write(data);
+		}
+
+		sc_stop;
+		break;
 	}
 }
