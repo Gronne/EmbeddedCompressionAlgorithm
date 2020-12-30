18a81e39d7a041980a6d0389f1d4db4f04da7003
Gronne <mathias0909@gmail.com>
2020-12-30 08:36:18 +0100

    Added the general flow to PredictiveSetup, functions have been created but not implemented. This will make it easier to follow what is happening

--- Compression/ComplexCompression/PredictiveSetup.h
+++ Compression/ComplexCompression/PredictiveSetup.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ICompressionSetup.h"
 #include <vector>
+#include <array>
 
 template<class SensorT, class ModelT>
 class PredictiveSetup : public ICompressionSetup<SensorT, ModelT>
@@ -9,6 +10,9 @@ public:
 	PredictiveSetup() {};
 	~PredictiveSetup() {};
 
+	// ------------------------------------------------------------------------
+	// !!!!! SHOULD BE MADE FROM VECTOR TO ARRAYS WITH TEMPLATES OR CONST !!!!!
+	// ------------------------------------------------------------------------
 
 
 	bool setup(SensorT dataPoint) { 
@@ -19,32 +23,59 @@ public:
 		if (detectPeriode(&_periodeData) == false)
 			return false;
 		
-		//Create autoCorrelation matrix and vector
-
+		//Create autoCorrelation matrix and vector. 3 to keep it simple
+		const int predictionSize = 3; 
+		std::vector<SensorT> autoVector = calcAutoVec(&_periodeData, predictionSize);	
+		std::vector<SensorT> autoMatrix = createAutoMatrix(&autoVector, predictionSize);
 
 		//Invert matrix
-
+		std::vector<SensorT> invMatrix = inverseMatrix(&autoMatrix);
 
 		//multiply inverted matrix and vector to get coefficients
-
-		
-		// Save coefficients as the model
-
+		_coefficients = calcCoefficients(&invMatrix, &autoVector);
 
 		//Return True
 		return true;
 	};
 
 
-	ModelT* getModel() { return new ModelT(); };
+	ModelT* getModel() { return &_coefficients; };
 
 
 private:
-
 	bool detectPeriode(std::vector<SensorT> *periodeData) {
 		return periodeData->size() > 10;
 	};
 
 
+	std::vector<SensorT> calcAutoVec(std::vector<SensorT>* periodeData, int length) {
+		std::vector<SensorT> autoVec;
+
+		return autoVec;
+	}
+
+
+	std::vector<SensorT> createAutoMatrix(std::vector<SensorT>* autoVec, int length) {
+		std::vector<SensorT> autoMatrix;
+
+		return autoMatrix;
+	}
+
+
+	std::vector<SensorT> inverseMatrix(std::vector<SensorT>* autoMatrix) {
+		std::vector<SensorT> invMatrix;
+
+		return invMatrix;
+	}
+
+
+	ModelT calcCoefficients(std::vector<SensorT>* invMatrix, std::vector<SensorT>* autoVecor) {
+		ModelT coefficients;
+
+		return coefficients;
+	}
+
+
 	std::vector<SensorT> _periodeData;
+	ModelT _coefficients;
 };
