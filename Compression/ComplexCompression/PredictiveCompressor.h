8a9edbc91964c956cae7870661eaa83c48b11ae1
Gronne <mathias0909@gmail.com>
2020-12-30 14:54:59 +0100

    Finished the predictive compression with a static matrix size of 3x3

--- Compression/ComplexCompression/PredictiveCompressor.h
+++ Compression/ComplexCompression/PredictiveCompressor.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "ICompressionCompress.h"
+#include <deque>
+#include <iostream>
 
 template<class DecompressT, class CompressT, class ModelT>
 class PredictiveCompressor : public ICompressionCompress<DecompressT, CompressT, ModelT>
@@ -9,14 +11,34 @@ public:
 	~PredictiveCompressor() {};
 
 	CompressT encode(DecompressT sensorData) { 
-		return CompressT(sensorData); 
+		//Predict point
+		DecompressT predictedPoint = predictPoint(_coefficients, &_dataBuffer);
+
+		//Calculate error
+		CompressT error = sensorData - predictedPoint;
+
+		//Push sensorData into list
+		_dataBuffer.push_front(sensorData);
+		_dataBuffer.pop_back();
+
+		//Return error
+		return error; 
 	};
 
 	void setModel(ICompressionSetup<DecompressT,ModelT> *setup) {
-
+		_coefficients = setup->getModel();
+		for (auto& e : *_coefficients)
+			_dataBuffer.push_front(0);
 	};
 
 private:
+	DecompressT predictPoint(ModelT *coefficients, std::deque<DecompressT> *dataBuffer) {
+		DecompressT newPoint = coefficients->at(0) * dataBuffer->at(0);
+		for (int index = 1; index < coefficients->size(); ++index)
+			newPoint = newPoint + coefficients->at(index) * dataBuffer->at(index);
+		return newPoint;
+	}
 
-
+	ModelT *_coefficients;
+	std::deque<DecompressT> _dataBuffer;
 };
